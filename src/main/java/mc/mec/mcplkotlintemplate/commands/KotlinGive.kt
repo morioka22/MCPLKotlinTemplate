74f4957a28fbc1990d1bc718b98b44088a465cc4
morioka22 <39086319+morioka22@users.noreply.github.com>
2020-03-07 11:35:29 +0900

    ミスを修正

--- src/main/java/mc/mec/mcplkotlintemplate/commands/KotlinGive.kt
+++ src/main/java/mc/mec/mcplkotlintemplate/commands/KotlinGive.kt
@@ -10,7 +10,7 @@ import org.bukkit.inventory.ItemStack
 object KotlinGive : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         (sender as? Player)?.inventory?.addItem(ItemStack(Material.STONE))
-        if ((sender as? Player) != null) {
+        if (sender !is Player) {
             sender.sendMessage("あなたはプレイヤーです。")
         } else {
             sender.sendMessage("あなたはプレイヤーではありません。")
